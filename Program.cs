96291e40e64404796fbd26b91e4499198b9c8d66
MotasemAlsuradi <alsuradym@gmail.com>
2025-02-16 02:37:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using LearningHub.Core.Common;
+using LearningHub.Core.Repositry;
+using LearningHub.Infra.Common;
+using LearningHub.Infra.Repository;
+using System.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Services for Domin layer
+builder.Services.AddScoped<IDbContext, DbContext>();
+
+//Services for Repository layer
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
